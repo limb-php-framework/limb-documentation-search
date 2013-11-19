7b24056aa9716a19d494402306cfd32a8140aa62
Резцов Андрей <rsa199@ya.ru>
2013-11-19 15:53:17 +0400

    HH-75 screwed MarkDown parser (pegdown)

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,30 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import java.io.File
+import scala.io.Source
+import org.apache.commons.io.FilenameUtils
+import org.apache.commons.io.FileUtils
+import scala.collection.JavaConversions._
+import org.pegdown.{PegDownProcessor, Extensions}
 
 object Application extends Controller {
+
+  def update() = Action {
+    val processor = new PegDownProcessor(Extensions.ALL)
+    val mdFiles = FileUtils.iterateFiles(new File("../limb"), Array("md"), true)
+    Ok(views.html.update(mdFiles.map { file =>
+      processor.parseMarkdown(Source.fromFile(file.toString).mkString.toCharArray)
+    }))
+  }
 
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
   }
+
+  def search(keywords: String) = Action {
+    Ok(views.html.search(keywords))
+  }
+
 
 }
--- build.sbt
+++ build.sbt
@@ -5,7 +5,10 @@ version := "1.0-SNAPSHOT"
 libraryDependencies ++= Seq(
   jdbc,
   anorm,
-  cache
+  cache,
+  "org.apache.commons" % "commons-io" % "1.3.2",
+  "eu.henkelmann" % "actuarius_2.10.0" % "0.2.6",
+  "org.pegdown" % "pegdown" % "1.0.1"
 )     
 
 play.Project.playScalaSettings
