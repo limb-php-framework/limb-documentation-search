7d428cda614ada3c5a4f44293506e932b9516c9a
Резцов Андрей <rsa199@ya.ru>
2013-12-18 14:41:20 +0400

    HH-83 update cron.d to retrieve data from the config

--- debian/limb-docs-searcher.cron.d
+++ debian/limb-docs-searcher.cron.d
@@ -1,2 +1,2 @@
- 0 0 * * sun curl -X POST http://0.0.0.0:9000/update?token=qwerty
+ 0 0 * * sun curl --data "token=`cat /etc/limb-docs-searcher/application.conf|grep token|awk '{print $3}'|sed 's/"//g'`" http://0.0.0.0:9000/update -X POST
  0 5 * * sun indexer --config /etc/limb-docs-indexer/sphinx.conf --all --rotate
