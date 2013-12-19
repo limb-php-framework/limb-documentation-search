0a8edcbfcd505425ea219a1f3ee5b519e664c602
knevcher <knevcher@gmail.com>
2013-12-19 16:15:54 +0400

    HH-88 fixes for standartization of sql init scripts

--- init/bootstrap.sql
+++ init/bootstrap.sql
@@ -1,5 +1,9 @@
-CREATE USER limb_docs_searcher WITH PASSWORD 'qwerty';
-CREATE DATABASE limb_docs_searcher;
-GRANT ALL PRIVILEGES ON DATABASE limb_docs_searcher to limb_docs_searcher;
+CREATE SCHEMA "limb-docs-searcher";
+SET search_path = 'limb-docs-searcher';
+
+CREATE USER "limb-docs-searcher" UNENCRYPTED PASSWORD '123';
+
+GRANT USAGE ON SCHEMA "limb-docs-searcher" TO "limb-docs-searcher";
+
 CREATE TABLE files (id SERIAL, url TEXT PRIMARY KEY, header TEXT, content TEXT);
 CREATE TABLE updates (id INTEGER PRIMARY KEY, timestamp TIMESTAMP DEFAULT NOW());
--- init/data.sql
+++ init/data.sql
@@ -1,1 +1,3 @@
+SET search_path = "limb-docs-searcher";
+
 INSERT INTO updates (id, timestamp) VALUES (1, ('1970-01-01 00:00:00+00'::timestamptz));
