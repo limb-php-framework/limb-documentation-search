c464d2916c7e5aef5208ed08c7794ae05633e693
Резцов Андрей <rsa199@ya.ru>
2013-12-24 12:21:27 +0400

    HH-88 update bootstrap.sql for fixing errors in the rights

--- init/bootstrap.sql
+++ init/bootstrap.sql
@@ -4,6 +4,8 @@ SET search_path = 'limb-docs-searcher';
 CREATE USER "limb-docs-searcher" UNENCRYPTED PASSWORD '123';
 
 GRANT USAGE ON SCHEMA "limb-docs-searcher" TO "limb-docs-searcher";
+GRANT SELECT, UPDATE, INSERT ON ALL TABLES IN SCHEMA "limb-docs-searcher" TO "limb-docs-searcher"
+GRANT all privileges ON SEQUENCE files_id_seq TO "limb-docs-searcher";
 
 CREATE TABLE files (id SERIAL, url TEXT PRIMARY KEY, header TEXT, content TEXT);
 CREATE TABLE updates (id INTEGER PRIMARY KEY, timestamp TIMESTAMP DEFAULT NOW());
