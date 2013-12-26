833c0d912b3f4bbc7e20603976f9be92ac1fe3ee
knevcher <knevcher@gmail.com>
2013-12-26 12:59:18 +0400

    HH-83 add delete permission on all tables in schema

--- init/bootstrap.sql
+++ init/bootstrap.sql
@@ -11,6 +11,6 @@ GRANT USAGE ON SCHEMA "limb-docs-searcher" TO "limb-docs-searcher";
 CREATE TABLE files (id SERIAL, url TEXT PRIMARY KEY, header TEXT, content TEXT);
 CREATE TABLE updates (id INTEGER PRIMARY KEY, timestamp TIMESTAMP DEFAULT NOW());
 
-GRANT SELECT, UPDATE, INSERT ON ALL TABLES IN SCHEMA "limb-docs-searcher" TO "limb-docs-searcher";
+GRANT SELECT, UPDATE, INSERT, DELETE ON ALL TABLES IN SCHEMA "limb-docs-searcher" TO "limb-docs-searcher";
 
-GRANT all privileges ON SEQUENCE files_id_seq TO "limb-docs-searcher";
+GRANT ALL PRIVILEGES ON SEQUENCE files_id_seq TO "limb-docs-searcher";
