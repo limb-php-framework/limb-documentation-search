a237b8221a939f1a1a01a95d79ff9600831958da
Резцов Андрей <rsa199@ya.ru>
2013-12-26 08:56:28 +0400

    HH-83 update bootstrap.sql

--- init/bootstrap.sql
+++ init/bootstrap.sql
@@ -1,11 +1,15 @@
+CREATE DATABASE "limb-docs-searcher";
+\connect limb-docs-searcher;
 CREATE SCHEMA "limb-docs-searcher";
 SET search_path = 'limb-docs-searcher';
 
 CREATE USER "limb-docs-searcher" UNENCRYPTED PASSWORD '123';
 
 GRANT USAGE ON SCHEMA "limb-docs-searcher" TO "limb-docs-searcher";
-GRANT SELECT, UPDATE, INSERT ON ALL TABLES IN SCHEMA "limb-docs-searcher" TO "limb-docs-searcher"
-GRANT all privileges ON SEQUENCE files_id_seq TO "limb-docs-searcher";
 
 CREATE TABLE files (id SERIAL, url TEXT PRIMARY KEY, header TEXT, content TEXT);
 CREATE TABLE updates (id INTEGER PRIMARY KEY, timestamp TIMESTAMP DEFAULT NOW());
+
+GRANT SELECT, UPDATE, INSERT ON ALL TABLES IN SCHEMA "limb-docs-searcher" TO "limb-docs-searcher";
+
+GRANT all privileges ON SEQUENCE files_id_seq TO "limb-docs-searcher";
