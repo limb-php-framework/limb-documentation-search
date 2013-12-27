48bf7894fffd3e03f3d1a753a937929ed3f0299d
knevcher <knevcher@gmail.com>
2013-12-27 16:07:35 +0400

    HH-93 - minor fixes at nginx config example

--- nginx.conf.ex
+++ nginx.conf.ex
@@ -3,18 +3,19 @@ upstream limbdocssearcher {
 }
 
 server {
+       listen 80;
+
        access_log /var/log/limb-docs-searcher/nginx.access.log;
        error_log  /var/log/limb-docs-searcher/nginx.error.log;
+
        proxy_buffering    off;
+
        proxy_set_header   X-Real-IP $remote_addr;
        proxy_set_header   X-Scheme $scheme;
        proxy_set_header   X-Forwarded-For $proxy_add_x_forwarded_for;
        proxy_set_header   Host $http_host;
-       listen 80;
 
-       location /assets/* {
-               proxy_pass  http://192.168.0.100;
-       }
+
        location / {
                proxy_pass  http://limbdocssearcher;
                proxy_read_timeout 5s;
