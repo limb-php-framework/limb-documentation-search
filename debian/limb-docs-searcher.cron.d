b9d335ff9545633bcf09fe05fc6037fc374a726a
Резцов Андрей <rsa199@ya.ru>
2013-12-30 12:35:25 +0400

    HH-93 replace 0.0.0.0 to localhost

--- debian/limb-docs-searcher.cron.d
+++ debian/limb-docs-searcher.cron.d
@@ -1,2 +1,2 @@
- 0 0 * * *  curl --data "token=`cat /etc/limb-docs-searcher/application.conf|grep token|awk '{print $3}'|sed 's/"//g'`" http://0.0.0.0:`cat /etc/default/limb-docs-searcher|grep PORT|sed 's/PORT=//g'`/update -X POST
+ 0 0 * * *  curl --data "token=`cat /etc/limb-docs-searcher/application.conf|grep token|awk '{print $3}'|sed 's/"//g'`" http://localhost:`cat /etc/default/limb-docs-searcher|grep PORT|sed 's/PORT=//g'`/update -X POST
  0 5 * * *  indexer --config /etc/limb-docs-searcher/sphinx.conf --all --rotate
