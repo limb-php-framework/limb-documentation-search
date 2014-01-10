7a63f5181e72c556be271f06793cfc157d188388
Резцов Андрей <rsa199@ya.ru>
2014-01-10 13:10:07 +0400

    HH-91 added the ability to overwrite the settings in the config file conf/application.development.conf

--- app/controllers/Global.scala
+++ app/controllers/Global.scala
@@ -2,6 +2,8 @@ import play.api._
 import play.api.mvc._
 import play.api.mvc.Results._
 import scala.concurrent.Future
+import java.io.File
+import com.typesafe.config.ConfigFactory
 
 object Global extends GlobalSettings {
 
@@ -23,6 +25,13 @@ object Global extends GlobalSettings {
 
   override def onBadRequest(request: RequestHeader, error: String) = {
     Future.successful(BadRequest("Bad Request"))
+  }
+
+  override def onLoadConfig(config: Configuration, path: File, classloader: ClassLoader, mode: Mode.Mode): Configuration = {
+    val modeSpecificConfig = config ++ Configuration(ConfigFactory.load("application.development.conf"))
+    Logger.debug("config loaded")
+    super.onLoadConfig(modeSpecificConfig, path, classloader, mode)
+
   }
 
 }
