987de40b328f7cae8ccfcbf7b04b9f0667e29544
Резцов Андрей <rsa199@ya.ru>
2014-01-14 13:02:28 +0400

    HH-73 added development example config

---
+++ conf/application.dev.conf.ex
@@ -0,0 +1,22 @@
+application.secret="GO28;2pShFx`q_Q``s_ArE<Fkvwd/EfPFLxxoCM7NlA[Bt1IvD]B[W81LWXi]bhU"
+
+application.langs="en"
+
+last_modified_file = "/var/lib/limb-docs-searcher/updates"
+page_results = 10
+secret_token_for_indexing = "c97cb3780aea83367e3fa89d1173e687"
+sphinx_server = localhost
+sphinx_port = 9312
+index_name = "limb"
+download_limb_if_not_exists = true
+limb_git_path = "https://github.com/limb-php-framework/limb.git"
+limb_local_path = "/var/lib/limb-docs-searcher/limb/"
+update_limb_local_repo = true
+count_snippets = 20
+static_url = "http://localhost:9000/assets/"
+
+db.default.driver=org.postgresql.Driver
+db.default.user=limb-docs-searcher
+db.default.password=123
+db.default.url="jdbc:postgresql://localhost:5432/limb-docs-searcher"
+db.default.acquireRetryAttempts=10
